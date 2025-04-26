5c24e1dad9bbf2a92d22ab62019abf18b7553d61
Rahul Chauhan <rahulchauhan962103@gmail.com>
2025-04-26 23:14:54 +0530

    Updating Cart component

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { NavLink } from 'react-router-dom'
+import CartItem from '../components/CartItem'
+import { useState } from 'react'
 
 const Cart = () => {
+  const {cart}=useSelector((state)=>state)
+  const [TotalAmount, setTotalAmount] = useState(0)
+
+
+  useEffect(() => {
+    const total = cart.reduce((acc, curr) => acc + curr, 0);
+    setTotalAmount(total);
+  }, [cart]);
+  
+  
   return (
-    <div>Cart</div>
+    <div>
+      <div>
+        {
+          Cart.length > 0 ?
+          (
+            <div>
+              <div>
+                {
+                  cart.map((item,index)=>{
+                    return <CartItem key={cart.id} item={item} cartIndex={index}/>
+                  })
+                }
+              </div>
+            </div>
+          ):
+          (
+            <div>
+              <h1>Cart Empty</h1>
+              <NavLink to="/">
+              <button>Shop Now</button>
+              </NavLink>
+            </div>
+          )
+        }
+      </div>
+
+      <div>
+        <div>
+          <p>YOUR CART</p>
+          <h1>Order Summary</h1>
+          <span>Total Item: {cart.length}</span>
+        </div>
+
+        <div>
+          <span>Total Amount:{TotalAmount}</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
